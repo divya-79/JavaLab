a721442facc71f02541311bc48eed33f8aa7a391
divya-79 <goeldivyanshi99@gmail.com>
2026-02-06 13:14:36 +0530

    Program1
    This program demonstrates a basic calculator in java using the command line argument and type casting concepts.

---
+++ cla.java
@@ -0,0 +1,27 @@
+public class cla {
+    static int add(int num1, int num2) { // num1,num2 are integers to add
+        return num1 + num2;
+    }
+
+    static int sub(int num1, int num2) { // num1,num2 are integers to subtract
+        return num1 - num2;
+    }
+
+    static int mul(int num1, int num2) { // num1,num2 are integers to multiply
+        return num1 * num2;
+    }
+
+    static int div(int num1, int num2) { // num1,num2 are integers to divide
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]); // narrowing of atring arguments into integer arguments
+        int n2 = Integer.parseInt(args[1]); // narrowing of atring arguments into integer arguments
+        System.out.println("addition of command line arguments are " + add(n1, n2));
+        System.out.println("subtraction of command line arguments are " + sub(n1, n2));
+        System.out.println("multiplication of command line arguments are " + mul(n1, n2));
+        System.out.println("division of command line arguments are " + div(n1, n2));
+
+    }
+}
